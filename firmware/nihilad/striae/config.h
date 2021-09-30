3a23d98cb2cf5c38c60a083bcdd226d5862495c1
straef <20950411+straef@users.noreply.github.com>
2021-09-30 15:41:56 -0500

    started readme. slight changes to config.h

--- firmware/nihilad/striae/config.h
+++ firmware/nihilad/striae/config.h
@@ -1,16 +1,21 @@
 //copyright 2021 straef <straef@nihilad.com>
 
-#define VENDOR_ID					0x0010
+#define VENDOR_ID				0x0010
 #define PRODUCT_ID				0x0110
 #define DEVICE_VER				0X0000
+
 #define MANUFACTURER			hexRAVEN_labs
-#define PRODUCT						striae_r00
+#define PRODUCT					striae_r00
+#define DESCRIPTION				the striae r00 by hexRaven_labs is a fullsize plus ortholinear keyboard with a left-side numpad and nav cluster
 
 #define MATRIX_ROWS				12
 #define MATRIX_COLS				10
 
-#define MATRIX_ROW_PINS		{ A0, A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11 }
-#define MATRIX_COL_PINS		{ B0, B1, B2, B3, B4, B5, B8, B9, B10, B12 }
+#define MATRIX_ROW_PINS			{ A0, A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, A11 }
+#define MATRIX_COL_PINS			{ B0, B1, B2, B3, B4, B5, B8, B9, B10, B12 }
 #define UNUSED_PINS				{}
 
-#define DIODE_DIRECTION		COL2ROW
+#define DIODE_DIRECTION			COL2ROW
+
+#define BOOTMAGIC_LITE_ROW		0
+#define BOOTMAGIC_LITE_COLUMN	3
