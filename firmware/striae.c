d73ebba653a98d11177f33fbddea6ca75ceb44bd
straef <20950411+straef@users.noreply.github.com>
2022-04-19 22:12:41 -0500

    sundry

--- firmware/striae.c
+++ firmware/striae.c
@@ -4,3 +4,15 @@ void matrix_init_kb(void);
 void matrix_scan_kb(void);
 bool process_record_kb(uint16_t keycode, keyrecord_t *record);
 void led_set_kb(uint8_t usb_led);
+
+/* code by @drashna#0343 to let the "USER" key (connected to pin A0) work as a reset button for the board
+void keyboard_pre_init_user(void) {
+    setPinInputHigh(A0);
+}
+
+void matrix_scan_user(void) {
+    if (!readPin(A0)) {
+        reset_keyboard();
+    }
+}
+*/
